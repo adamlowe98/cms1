803937e221811b07b794cf60cdfbeafb3c76e091
Adam <adamlowe1998@gmail.com>
2024-11-13 23:30:23 +0000

    Update courses.js

--- pages/courses.js
+++ pages/courses.js
@@ -1,11 +1,14 @@
 import { gql } from "@apollo/client";
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/header";
+import EntryHeader from "../components/entry-header";
 import Footer from "../components/footer";
-import style from "../styles/courses.module.css";
+import style from "../styles/front-page.module.css";
 
 export default function Courses(props) {
-  const { title: siteTitle } = props.data.generalSettings;
+  const { title: siteTitle, description: siteDescription } = props.data.generalSettings;
+  const menuItems = props.data.primaryMenuItems.nodes;
 
   return (
     <>
@@ -13,12 +16,16 @@ export default function Courses(props) {
         <title>{siteTitle} - Courses</title>
       </Head>
 
-      <Header siteTitle={siteTitle} />
+      <Header siteTitle={siteTitle} siteDescription={siteDescription} menuItems={menuItems} />
 
       <main className="container">
-        <h1>Courses Management</h1>
-        <p>Manage course details, including adding, editing, and deleting courses.</p>
-        {/* Add course management functionalities here */}
+        <EntryHeader title="Course Management" />
+
+        <section className={style.cardGrid}>
+          <h3>Manage Courses</h3>
+          <p>Here you can create, update, and delete courses.</p>
+          {/* Additional course management features */}
+        </section>
       </main>
 
       <Footer />
@@ -27,9 +34,8 @@ export default function Courses(props) {
 }
 
 Courses.query = gql`
+  ${Header.fragments.entry}
   query GetCoursesPage {
-    generalSettings {
-      title
-    }
+    ...HeaderFragment
   }
 `;
