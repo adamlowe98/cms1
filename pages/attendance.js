b3511b7e749a7aeda87ef7c63a8f34dbf6dbadfa
Adam <adamlowe1998@gmail.com>
2024-11-13 23:31:06 +0000

    Update attendance.js

--- pages/attendance.js
+++ pages/attendance.js
@@ -1,11 +1,14 @@
 import { gql } from "@apollo/client";
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/header";
+import EntryHeader from "../components/entry-header";
 import Footer from "../components/footer";
-import style from "../styles/attendance.module.css";
+import style from "../styles/front-page.module.css";
 
 export default function Attendance(props) {
-  const { title: siteTitle } = props.data.generalSettings;
+  const { title: siteTitle, description: siteDescription } = props.data.generalSettings;
+  const menuItems = props.data.primaryMenuItems.nodes;
 
   return (
     <>
@@ -13,23 +16,19 @@ export default function Attendance(props) {
         <title>{siteTitle} - Attendance</title>
       </Head>
 
-      <Header siteTitle={siteTitle} />
+      <Header siteTitle={siteTitle} siteDescription={siteDescription} menuItems={menuItems} />
 
       <main className="container">
-        <h1>Attendance Management</h1>
-        <p>Record and monitor student attendance effectively.</p>
-        {/* Add attendance management functionalities here */}
+        <EntryHeader title="Attendance Management" />
+
+        <section className={style.cardGrid}>
+          <h3>Manage Attendance</h3>
+          <p>Here you can track and manage student attendance.</p>
+          {/* Additional attendance management features */}
+        </section>
       </main>
 
       <Footer />
     </>
   );
 }
-
-Attendance.query = gql`
-  query GetAttendancePage {
-    generalSettings {
-      title
-    }
-  }
-`;
