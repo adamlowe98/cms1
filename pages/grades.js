ee4beac29632479ff9f8620639f6c225a2cec368
Adam <adamlowe1998@gmail.com>
2024-11-13 23:30:48 +0000

    Update grades.js

--- pages/grades.js
+++ pages/grades.js
@@ -1,11 +1,14 @@
 import { gql } from "@apollo/client";
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/header";
+import EntryHeader from "../components/entry-header";
 import Footer from "../components/footer";
-import style from "../styles/grades.module.css";
+import style from "../styles/front-page.module.css";
 
 export default function Grades(props) {
-  const { title: siteTitle } = props.data.generalSettings;
+  const { title: siteTitle, description: siteDescription } = props.data.generalSettings;
+  const menuItems = props.data.primaryMenuItems.nodes;
 
   return (
     <>
@@ -13,12 +16,16 @@ export default function Grades(props) {
         <title>{siteTitle} - Grades</title>
       </Head>
 
-      <Header siteTitle={siteTitle} />
+      <Header siteTitle={siteTitle} siteDescription={siteDescription} menuItems={menuItems} />
 
       <main className="container">
-        <h1>Grades Management</h1>
-        <p>Track and update student grades efficiently.</p>
-        {/* Add grade management functionalities here */}
+        <EntryHeader title="Grade Management" />
+
+        <section className={style.cardGrid}>
+          <h3>Manage Grades</h3>
+          <p>Here you can input and modify student grades.</p>
+          {/* Additional grade management features */}
+        </section>
       </main>
 
       <Footer />
@@ -27,9 +34,8 @@ export default function Grades(props) {
 }
 
 Grades.query = gql`
+  ${Header.fragments.entry}
   query GetGradesPage {
-    generalSettings {
-      title
-    }
+    ...HeaderFragment
   }
 `;
