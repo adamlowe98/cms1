bce41ae0173d91d31cb070d9a0b906a896a64785
Adam <adamlowe1998@gmail.com>
2024-11-14 00:40:51 +0000

    Update front-page.js

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -17,8 +17,7 @@ export default function Home(props) {
         <title>{siteTitle}</title>
       </Head>
 
-           <Header menuItems={menuItems} />
-
+ <Header siteTitle={siteTitle} siteDescription={siteDescription} menuItems={menuItems} />
 
       <main className="container">
         <EntryHeader title="Student Management System" />
