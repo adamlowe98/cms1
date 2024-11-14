f7bac9ad045beef9f6194482daf49ed4b43b0d78
Adam <adamlowe1998@gmail.com>
2024-11-14 00:51:59 +0000

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,9 +3,7 @@ import styles from "./footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      Powered by{" "}
-thewordpressdoctor69      
-        </a>
+      Powered by thewordpressdoctor69
     </footer>
   );
 }
