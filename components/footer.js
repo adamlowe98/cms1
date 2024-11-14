3417980728118b09dda29db492f3c46637c0c1ea
Adam <adamlowe1998@gmail.com>
2024-11-14 00:48:31 +0000

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -4,9 +4,8 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       Powered by{" "}
-      <a href="https://wpengine.com" target="_blank" rel="noopener noreferrer">
-        WP Engine
-      </a>
+thewordpressdoctor69      
+        </a>
     </footer>
   );
 }
