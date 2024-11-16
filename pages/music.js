c432189ad3815c3bca01645c93f47110b06d1ab9
Adam <adamlowe1998@gmail.com>
2024-11-16 13:37:19 +0000

    Update music.js

--- pages/music.js
+++ pages/music.js
@@ -11,50 +11,30 @@ const MusicPlayer = () => {
   const { title: siteTitle, description: siteDescription } = data.generalSettings;
   const menuItems = data.primaryMenuItems.nodes;
 
+  const musicFiles = [
+    { name: 'Song 1', src: 'path/to/song1.mp3', thumbnail: 'path/to/thumbnail1.jpg' },
+    { name: 'Song 2', src: 'path/to/song2.mp3', thumbnail: 'path/to/thumbnail2.jpg' },
+    // Add more songs as needed
+  ];
+
   return (
     <>
       <Head>
-        <title>{siteTitle} - RatPack Music</title>
+        <title>{siteTitle} - Students</title>
       </Head>
-
       <Header menuItems={menuItems} />
-    
-      <main className="container">
-        <EntryHeader title="The RatPack Music" />
-
-        <nav className={style.fancyMenu}>
-          <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/students">Students</Link></li>
-            <li><Link href="/courses">Courses</Link></li>
-            <li><Link href="/grades">Grades</Link></li>
-            <li><Link href="/attendance">Attendance</Link></li>
-            <li><Link href="/glenn-mannion">Glenn Mannion</Link></li>
-          </ul>
-        </nav>
-    
-        const musicFiles = [
-          { name: 'Song 1', src: 'path/to/song1.mp3', thumbnail: 'path/to/thumbnail1.jpg' },
-          { name: 'Song 2', src: 'path/to/song2.mp3', thumbnail: 'path/to/thumbnail2.jpg' },
-          // Add more songs as needed
-        ];
-
-        return (
-          <main className={style.musicContainer}>
-            {musicFiles.map((file, index) => (
-              <div key={index} className={style.musicItem}>
-                <img src={file.thumbnail} alt={file.name} className={style.thumbnail} />
-                <h4>{file.name}</h4>
-                <audio controls>
-                  <source src={file.src} type="audio/mp3" />
-                  Your browser does not support the audio element.
-                </audio>
-              </div>
-            ))}
-          </main>
-        );
-      };
-
+      <main className={style.musicContainer}>
+        {musicFiles.map((file, index) => (
+          <div key={index} className={style.musicItem}>
+            <img src={file.thumbnail} alt={file.name} className={style.thumbnail} />
+            <h4>{file.name}</h4>
+            <audio controls>
+              <source src={file.src} type="audio/mp3" />
+              Your browser does not support the audio element.
+            </audio>
+          </div>
+        ))}
+      </main>
       <Footer />
     </>
   );
