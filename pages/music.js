851c94f13fe3dc77654e79c8aac82e502f35d400
Adam <adamlowe1998@gmail.com>
2024-11-16 13:26:47 +0000

    Update music.js

--- pages/music.js
+++ pages/music.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Head from 'next/head';
+import Header from '../components/Header';
 import style from '../styles/front-page.module.css';
+
+const siteTitle = 'RatPack Music';
+const menuItems = ['Home', 'About', 'Music', 'Contact'];
 
 const musicFiles = [
   { name: 'Song 1', src: 'path/to/song1.mp3', thumbnail: 'path/to/thumbnail1.jpg' },
@@ -7,41 +12,26 @@ const musicFiles = [
   // Add more songs as needed
 ];
 
-<Head>
-        <title>{siteTitle} - Students</title>
-      </Head>
-
-      <Header menuItems={menuItems} />
-
-      
-      <main className="container">
-        <EntryHeader title="RatPack Music" />
-
-    <nav className={style.fancyMenu}>
-          <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/students">Students</Link></li>
-            <li><Link href="/courses">Courses</Link></li>
-            <li><Link href="/grades">Grades</Link></li>
-            <li><Link href="/attendance">Attendance</Link></li>
-    <li><Link href="/glenn-mannion">Glenn Mannion</Link></li>
-          </ul>
-        </nav>
-
 const MusicPlayer = () => {
   return (
-    <div className={style.musicContainer}>
-      {musicFiles.map((file, index) => (
-        <div key={index} className={style.musicItem}>
-          <img src={file.thumbnail} alt={file.name} className={style.thumbnail} />
-          <h4>{file.name}</h4>
-          <audio controls>
-            <source src={file.src} type="audio/mp3" />
-            Your browser does not support the audio element.
-          </audio>
-        </div>
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>{siteTitle} - Students</title>
+      </Head>
+      <Header menuItems={menuItems} />
+      <main className={style.musicContainer}>
+        {musicFiles.map((file, index) => (
+          <div key={index} className={style.musicItem}>
+            <img src={file.thumbnail} alt={file.name} className={style.thumbnail} />
+            <h4>{file.name}</h4>
+            <audio controls>
+              <source src={file.src} type="audio/mp3" />
+              Your browser does not support the audio element.
+            </audio>
+          </div>
+        ))}
+      </main>
+    </>
   );
 };
 
